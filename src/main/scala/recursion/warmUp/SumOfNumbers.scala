6f80e4dcaa81bee355957d92460619cef1a38755
dev-navian <ivan.annan@turntabl.io>
2022-08-09 22:57:14 +0000

    sum of numbers between two integers tail recursion

--- src/main/scala/recursion/warmUp/SumOfNumbers.scala
+++ src/main/scala/recursion/warmUp/SumOfNumbers.scala
@@ -1,15 +1,27 @@
 package io.turntabl
 package recursion.warmUp
 
-object SumOfNumbers {
-  def main(args: Array[String]): Unit = {
-    val res = sumNumbers(1, 5)
-    println(res)
-  }
+import scala.annotation.tailrec
 
+object SumOfNumbers extends App {
+
+  // ordinary recursion
   def sumNumbers(numOne: Int, numTwo: Int): Int = {
     if (numOne + 1 >= numTwo) 0
 
     else (numOne + 1) + sumNumbers(numOne + 1, numTwo)
+  }
+
+  // with tail recursion
+  def sumNumbersTailRecursion(numOne: Int, numTwo: Int): Int = {
+
+    @tailrec
+    def sumOfNumbers(numOne: Int, numTwo: Int, acc: Int): Int  = {
+      if (numOne + 1 >= numTwo) acc
+
+      else sumOfNumbers((numOne + 1), numTwo, (numOne + 1) + acc);
+    }
+
+    sumOfNumbers(numOne, numTwo, 0);
   }
 }
--- src/test/scala/recursion/warmUp/SumOfNumbersTest.scala
+++ src/test/scala/recursion/warmUp/SumOfNumbersTest.scala
@@ -5,6 +5,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class SumOfNumbersTest extends AnyFunSuite {
 
+  // tests for ordinary recursion
   test("Sum of integers between 1 and 5 equals 9") {
     val result = SumOfNumbers.sumNumbers(1, 5);
     assert(result == 9);
@@ -19,4 +20,22 @@ class SumOfNumbersTest extends AnyFunSuite {
     val result = SumOfNumbers.sumNumbers(2, 3);
     assert(result == 0)
   }
+
+
+  // tests for tail recursion
+  test("Tail recursion: Sum of integers between 1 and 5 equals 9") {
+    val result = SumOfNumbers.sumNumbersTailRecursion(1, 5);
+    assert(result == 9);
+  }
+
+  test("Tail recursion: Sum of integers between 4 and 8 equals 18") {
+    val result = SumOfNumbers.sumNumbersTailRecursion(4, 8);
+    assert(result != 20)
+  }
+
+  test("Tail recursion: Sum of integers between 2 and 3 equals 0") {
+    val result = SumOfNumbers.sumNumbersTailRecursion(2, 3);
+    assert(result == 0)
+  }
+
 }
