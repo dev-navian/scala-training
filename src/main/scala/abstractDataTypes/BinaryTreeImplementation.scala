5f18a63a75651c0fe02245a2ed70d050cd7e1859
dev-navian <ivan.annan@turntabl.io>
2022-10-12 14:13:33 +0000

    size method added

--- src/main/scala/abstractDataTypes/BinaryTreeImplementation.scala
+++ src/main/scala/abstractDataTypes/BinaryTreeImplementation.scala
@@ -1,11 +1,14 @@
 package io.turntabl
 package abstractDataTypes
 
+import scala.annotation.tailrec
+
 // trait with contravariance behaviour
 sealed trait BinaryTree[+T] {
   def value: T
   def left: BinaryTree[T]
   def right: BinaryTree[T]
+  def size: Int
 }
 
 // an Empty Node is also known as a leaf
@@ -15,13 +18,42 @@ case object Leaf extends BinaryTree[Nothing] {
   override def left: BinaryTree[Nothing] = throw new NoSuchElementException("Error accessing the left child of an empty node")
 
   override def right: BinaryTree[Nothing] = throw new NoSuchElementException("Error accessing the right child of an empty node")
+
+  override def size: Int = 0
 }
 
 // a Node has is made up of three parts: the element itself, a left child node and a right child node
-case class Branch[T](l: BinaryTree[T], v: T, r: BinaryTree[T]) extends BinaryTree[T] {
+case class Branch[T](v: T, l: BinaryTree[T], r: BinaryTree[T]) extends BinaryTree[T] {
   override def value: T = v
 
   override def left: BinaryTree[T] = l
 
   override def right: BinaryTree[T] = r
+
+  override def size: Int = {
+    def calculateSize(node: BinaryTree[T]): Int = node match {
+      case Leaf => 1
+      case Branch(v, l , r) => 1 + calculateSize(l) + calculateSize(r)
+    }
+
+    calculateSize(this)
+  }
+}
+
+object BinaryTree {
+
+  def main(args: Array[String]): Unit = {
+    val firstLeaf = Leaf;
+
+    val firstBranch: BinaryTree[Int] = Branch(
+        2,
+      Branch(3, Leaf, Leaf),
+      Branch(4, Branch(5, Leaf, Leaf), Leaf)
+    )
+    println(firstBranch)
+    println(firstBranch.size)
+
+  }
+
+
 }
